6b461bb88127f99223bc3adb839aceec76377e87
Amsozzer1 <asozzer3@student.ccc.edu>
2023-03-30 09:03:16 -0500

    done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
+import java.io.File;  // Import the File class
+import java.io.FileWriter;
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        File myObj = new File("help.csv"); // Specify the filename
+        FileWriter write = new FileWriter(myObj);
+        Scanner sc = new Scanner(myObj);
+        myObj.setWritable(true);
+        System.out.println(myObj.exists());
+        while(sc.hasNextLine())
+        {
+            String line = sc.nextLine();
+            String ints[] = line.split(",");
+            write.append(line);
+            for(int i=0; i< ints.length; i++)
+            {
+                System.out.println("Int: " + ints[i]+ "\n");
+                
+
+            }
+            
+        }
+
+        
+        
+      sc.close();  
     }
 }
